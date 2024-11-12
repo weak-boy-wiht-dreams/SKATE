3958fc01e3870cb4ac816b9cfcecef7124f5fced
RoseLYi <161707875+RoseLYi@users.noreply.github.com>
2024-11-12 17:48:57 +0800

    Update tools.py

--- feeders/tools.py
+++ feeders/tools.py
@@ -1,375 +1,126 @@
 import random
 import numpy as np
 import math
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-
-def valid_crop_resize(data_numpy, valid_frame_num, p_interval, window, thres):
-    # input: C,T,V,M
-    C, T, V, M = data_numpy.shape
-    begin = 0
-    end = valid_frame_num
-    valid_size = end - begin
-
-    # crop
-    if len(p_interval) == 1:
-        p = p_interval[0]
-        bias = int((1 - p) * valid_size / 2)
-        data = data_numpy[:, begin + bias:end - bias, :, :]  # center_crop
-        cropped_length = data.shape[1]
-        c_b = begin + bias
-        c_e = end - bias
-    else:
-        p = np.random.rand(1) * (p_interval[1] - p_interval[0]) + p_interval[0]
-        cropped_length = np.minimum(np.maximum(int(np.floor(valid_size * p)), thres), valid_size)  # constraint cropped_length lower bound as thres
-        bias = np.random.randint(0, valid_size - cropped_length + 1)
-        data = data_numpy[:, begin + bias:begin + bias + cropped_length, :, :]
-        if data.shape[1] == 0:
-            print(cropped_length, bias, valid_size)
-        c_b = begin + bias
-        c_e = begin + bias + cropped_length
-
-    # resize
-    data = torch.tensor(data, dtype=torch.float)  # C, crop_t, V, M
-    data = data.permute(2, 3, 0, 1).contiguous().view(V * M, C, cropped_length)  # V*M, C, crop_t
-    data = F.interpolate(data, size=window, mode='linear', align_corners=False)  # V*M, C, T
-    data = data.contiguous().view(V, M, C, window).permute(2, 3, 0, 1).contiguous().numpy()
-    index_t = torch.arange(start=c_b, end=c_e, dtype=torch.float)
-    index_t = F.interpolate(index_t[None, None, :], size=window, mode='linear', align_corners=False).squeeze()
-    index_t = 2 * index_t / valid_size - 1
-    return data, index_t.numpy()
-
-
-def valid_crop_uniform(data_numpy, valid_frame_num, p_interval, window, thres):
-    # input: C,T,V,M
-    C, T, V, M = data_numpy.shape
-    begin = 0
-    end = valid_frame_num
-    valid_size = end - begin
-
-    # crop
-    if len(p_interval) == 1:
-        p = p_interval[0]
-        cropped_length = np.minimum(np.maximum(int(np.floor(valid_size * p)), thres), valid_size)
-        bias = int((1 - p) * valid_size / 2)
-
-        if cropped_length < window:
-            inds = np.arange(cropped_length)
+class FeederUtils:
+    @staticmethod
+    def crop_resize(data, valid_frames, p_interval, window, threshold):
+        C, T, V, M = data.shape
+        valid_size = valid_frames
+        if len(p_interval) == 1:
+            p = p_interval[0]
+            cropped_length = max(int(valid_size * p), threshold)
+            bias = int((1 - p) * valid_size / 2)
+            data = data[:, bias:bias + cropped_length, :, :]
         else:
-            bids = np.array(
-                [i * cropped_length // window for i in range(window + 1)])
-            bst = bids[:window]
-            inds = bst
-
-        inds = inds + bias
-        data = data_numpy[:, inds, :, :]
-
-    else:
-        p = np.random.rand(1) * (p_interval[1] - p_interval[0]) + p_interval[0]
-        cropped_length = np.minimum(np.maximum(int(np.floor(valid_size * p)), thres),
-                                    valid_size)  # constraint cropped_length lower bound as 64
-        bias = np.random.randint(0, valid_size - cropped_length + 1)
-
-        if cropped_length < window:
-            inds = np.arange(cropped_length)
-        elif window <= cropped_length < 2 * window:
-            basic = np.arange(window)
-            inds = np.random.choice(window + 1, cropped_length - window, replace=False)
-            offset = np.zeros(window + 1, dtype=np.int64)
-            offset[inds] = 1
-            offset = np.cumsum(offset)
-            inds = basic + offset[:-1]
-        else:
-            bids = np.array([i * cropped_length // window for i in range(window + 1)])
-            bsize = np.diff(bids)
-            bst = bids[:window]
-            offset = np.random.randint(bsize)
-            inds = bst + offset
-
-        inds = inds + bias
-        data = data_numpy[:, inds, :, :]
-        if data.shape[1] == 0:
-            print(cropped_length, bias, valid_size)
-
-    # resize
-    data = torch.tensor(data, dtype=torch.float)
-    index_t = torch.tensor(inds, dtype=torch.float)
-    data = data.permute(2, 3, 0, 1).contiguous().view(V * M, C, len(inds))  # V*M, C, crop_t
-
-    if len(inds) != window:
-        data = F.interpolate(data, size=window, mode='linear', align_corners=False)  # V*M, C, T
-        index_t = F.interpolate(index_t[None, None, :], size=window, mode='linear', align_corners=False).squeeze()
-
-    data = data.contiguous().view(V, M, C, window).permute(2, 3, 0, 1).contiguous().numpy()
-    index_t = 2 * index_t / valid_size - 1
-    return data, index_t.numpy()
-
-
-def scale(data_numpy, scale=0.2, p=0.5):
-    if random.random() < p:
-        scale = 1 + np.random.uniform(-1, 1, size=(3, 1, 1, 1)) * np.array(scale)
-        return data_numpy * scale
-    else:
-        return data_numpy.copy()
-
-
-transform_order = {'uav': [0, 2, 1, 4, 3, 6, 5, 8, 7, 10, 9, 12, 11, 14, 13, 16, 15]}
-
-def subtract(data_numpy, p=0.5):
-    joint = random.randint(0, 24)
-
-    C, T, V, M = data_numpy.shape
-    if random.random() < p:
-        data_numpy_new = np.zeros((C, T, V, M))
-        for i in range(V):
-            data_numpy_new[:, :, i, :] = data_numpy[:, :, i, :] - data_numpy[:, :, joint, :]
-        return data_numpy_new
-    else:
-        return data_numpy.copy()
-
-
-def temporal_flip(data_numpy, index_t, p=0.5):
-    C, T, V, M = data_numpy.shape
-    if random.random() < p:
-        time_range_order = [i for i in range(T)]
-        time_range_reverse = list(reversed(time_range_order))
-        return data_numpy[:, time_range_reverse, :, :], -index_t
-    else:
-        return data_numpy.copy(), index_t.copy()
-
-
-def spatial_flip(data_numpy, p=0.5):
-    if random.random() < p:
-        index = transform_order['uav']
-        return data_numpy[:, :, index, :]
-    else:
-        return data_numpy.copy()
-
-
-def rotate(data_numpy, axis=None, angle=None, p=0.5):
-    if axis != None:
-        axis_next = axis
-    else:
-        axis_next = random.randint(0, 2)
-
-    if angle != None:
-        angle_next = random.uniform(-angle, angle)
-    else:
-        angle_next = random.uniform(-30, 30)
-
-    if random.random() < p:
-        temp = data_numpy.copy()
-        angle = math.radians(angle_next)
-        # x
-        if axis_next == 0:
-            R = np.array([[1, 0, 0],
-                          [0, math.cos(angle), math.sin(angle)],
-                          [0, -math.sin(angle), math.cos(angle)]])
-        # y
-        if axis_next == 1:
-            R = np.array([[math.cos(angle), 0, -math.sin(angle)],
-                          [0, 1, 0],
-                          [math.sin(angle), 0, math.cos(angle)]])
-        # z
-        if axis_next == 2:
-            R = np.array([[math.cos(angle), math.sin(angle), 0],
-                          [-math.sin(angle), math.cos(angle), 0],
-                          [0, 0, 1]])
-        R = R.transpose()
-        temp = np.dot(temp.transpose([1, 2, 3, 0]), R)
-        temp = temp.transpose(3, 0, 1, 2)
-        return temp
-    else:
-        return data_numpy.copy()
-
-
-def shear(data_numpy, s1=None, s2=None, p=0.5):
-    if random.random() < p:
-        temp = data_numpy.copy()
-        if s1 != None:
-            s1_list = s1
-        else:
-            s1_list = [random.uniform(-0.5, 0.5), random.uniform(-0.5, 0.5), random.uniform(-0.5, 0.5)]
-        if s2 != None:
-            s2_list = s2
-        else:
-            s2_list = [random.uniform(-0.5, 0.5), random.uniform(-0.5, 0.5), random.uniform(-0.5, 0.5)]
-
-        R = np.array([[1, s1_list[0], s2_list[0]],
-                      [s1_list[1], 1, s2_list[1]],
-                      [s1_list[2], s2_list[2], 1]])
-        R = R.transpose()
-        temp = np.dot(temp.transpose([1, 2, 3, 0]), R)
-        temp = temp.transpose(3, 0, 1, 2)
-        return temp
-    else:
-        return data_numpy.copy()
-
-
-def drop_axis(data_numpy, axis=None, p=0.5):
-    if axis != None:
-        axis_next = axis
-    else:
-        axis_next = random.randint(0, 2)
-
-    if random.random() < p:
-        temp = data_numpy.copy()
-        C, T, V, M = data_numpy.shape
-        x_new = np.zeros((T, V, M))
-        temp[axis_next] = x_new
-        return temp
-    else:
-        return data_numpy.copy()
-
-
-def drop_joint(data_numpy, joint_list=None, time_range=None, p=0.5):
-    if random.random() < p:
-        temp = data_numpy.copy()
-        C, T, V, M = data_numpy.shape
-
-        if joint_list != None:
-            all_joints = [i for i in range(V)]
-            joint_list_ = random.sample(all_joints, joint_list)
-            joint_list_ = sorted(joint_list_)
-        else:
-            random_int = random.randint(5, 15)
-            all_joints = [i for i in range(V)]
-            joint_list_ = random.sample(all_joints, random_int)
-            joint_list_ = sorted(joint_list_)
-
-        if time_range != None:
-            all_frames = [i for i in range(T)]
-            time_range_ = random.sample(all_frames, time_range)
-            time_range_ = sorted(time_range_)
-        else:
-            random_int = random.randint(16, 32)
-            all_frames = [i for i in range(T)]
-            time_range_ = random.sample(all_frames, random_int)
-            time_range_ = sorted(time_range_)
-
-        x_new = np.zeros((C, len(time_range_), len(joint_list_), M))
-        temp2 = temp[:, time_range_, :, :].copy()
-        temp2[:, :, joint_list_, :] = x_new
-        temp[:, time_range_, :, :] = temp2
-        return temp
-    else:
-        return data_numpy.copy()
-
-
-def gaussian_noise(data_numpy, mean=0, std=0.05, p=0.5):
-    if random.random() < p:
-        temp = data_numpy.copy()
-        C, T, V, M = data_numpy.shape
-        noise = np.random.normal(mean, std, size=(C, T, V, M))
-        return temp + noise
-    else:
-        return data_numpy.copy()
-
-
-class GaussianBlurConv(nn.Module):
-    def __init__(self, channels=3, kernel=15, sigma=[0.1, 2], p=0.5):
-        super(GaussianBlurConv, self).__init__()
-        self.channels = channels
-        self.kernel = kernel
-        self.min_max_sigma = sigma
-        radius = int(kernel / 2)
-        self.kernel_index = np.arange(-radius, radius + 1)
-        self.p = p
-
-    def __call__(self, x):
-        sigma = random.uniform(self.min_max_sigma[0], self.min_max_sigma[1])
-        blur_flter = np.exp(-np.power(self.kernel_index, 2.0) / (2.0 * np.power(sigma, 2.0)))
-        kernel = torch.from_numpy(blur_flter).unsqueeze(0).unsqueeze(0)
-        # kernel =  kernel.float()
-        kernel = kernel.double()
-        kernel = kernel.repeat(self.channels, 1, 1, 1)
-        self.weight = nn.Parameter(data=kernel, requires_grad=False)
-
-        prob = np.random.random_sample()
-        x = torch.from_numpy(x).double()
-        if prob < self.p:
-            x = x.permute(3, 0, 2, 1)
-            x = F.conv2d(x, self.weight, padding=(0, int((self.kernel - 1) / 2)), groups=self.channels)
-            x = x.permute(1, -1, -2, 0)
-
-        return x.numpy()
-
-
-def gaussian_filter(data_numpy, kernel=15, sig_list=[0.1, 2], p=0.5):
-    g = GaussianBlurConv(3, kernel, sig_list, p)
-    return g(data_numpy)
-
-
-''' Skeleton AdaIN '''
-def skeleton_adain_bone_length(input, ref): # C T V M
-    eps = 1e-5
-    center = 1
-    ref_c = ref[:, :, center, :]
-
-    # joint to bone (joint2bone)
-    j2b = joint2bone()
-    bone_i = j2b(input) # C T V M
-    bone_r = j2b(ref)
-
-    bone_length_i = np.linalg.norm(bone_i, axis=0) # T V M
-    bone_length_r = np.linalg.norm(bone_r, axis=0)
-
-    bone_length_scale = (bone_length_r + eps) / (bone_length_i + eps) # T V M
-    bone_length_scale = np.expand_dims(bone_length_scale, axis=0) # 1 T V M
-
-    bone_i = bone_i * bone_length_scale
-
-    # bone to joint (bone2joint)
-    b2j = bone2joint()
-    joint = b2j(bone_i, ref_c)
-    return joint
-
-#换成我们的图节点（<16)
-class joint2bone(nn.Module):
-    def __init__(self):
-        super(joint2bone, self).__init__()
-        self.pairs = [(10, 8), (8, 6), (9, 7), (7, 5), (15, 13), (13, 11), (16, 14), (14, 12), (11, 5), (12, 6), (11, 12), (5, 6), (5, 0), (6, 0), (1, 0), (2, 0), (3, 1), (4, 2)]
+            p = np.random.uniform(p_interval[0], p_interval[1])
+            cropped_length = max(int(valid_size * p), threshold)
+            bias = np.random.randint(0, valid_size - cropped_length + 1)
+            data = data[:, bias:bias + cropped_length, :, :]
         
-    def __call__(self, joint):
-        bone = np.zeros_like(joint)
+        data = torch.tensor(data, dtype=torch.float)
+        data = F.interpolate(data.view(V * M, C, cropped_length).permute(1, 2, 0).unsqueeze(0), size=window, mode='linear', align_corners=False)
+        data = data.squeeze(0).permute(2, 0, 1).contiguous().view(C, window, V, M).numpy()
+        index_t = 2 * torch.linspace(bias, bias + cropped_length, window) / valid_size - 1
+        return data, index_t.numpy()
+
+    @staticmethod
+    def crop_uniform(data, valid_frames, p_interval, window, threshold):
+        C, T, V, M = data.shape
+        valid_size = valid_frames
+        p = np.random.uniform(p_interval[0], p_interval[1])
+        cropped_length = max(int(valid_size * p), threshold)
+        bias = np.random.randint(0, valid_size - cropped_length + 1)
+        indices = np.linspace(bias, bias + cropped_length - 1, window, dtype=int)
+        data = data[:, indices, :, :]
+        
+        data = torch.tensor(data, dtype=torch.float)
+        data = F.interpolate(data.view(V * M, C, len(indices)).permute(1, 2, 0).unsqueeze(0), size=window, mode='linear', align_corners=False)
+        data = data.squeeze(0).permute(2, 0, 1).contiguous().view(C, window, V, M).numpy()
+        index_t = 2 * torch.tensor(indices) / valid_size - 1
+        return data, index_t.numpy()
+
+class DataAugmentation:
+    @staticmethod
+    def scale(data, scale=0.2, prob=0.5):
+        return data * (1 + np.random.uniform(-scale, scale, size=(3, 1, 1, 1))) if random.random() < prob else data
+
+    @staticmethod
+    def spatial_flip(data, prob=0.5):
+        uav_order = [0, 2, 1, 4, 3, 6, 5, 8, 7, 10, 9, 12, 11, 14, 13, 16, 15]
+        return data[:, :, uav_order, :] if random.random() < prob else data
+
+    @staticmethod
+    def rotate(data, axis=None, angle=None, prob=0.5):
+        if random.random() >= prob:
+            return data
+        axis = axis or random.randint(0, 2)
+        angle = angle or random.uniform(-30, 30)
+        rotation_matrix = DataAugmentation._rotation_matrix(axis, math.radians(angle))
+        return np.dot(data.transpose(1, 2, 3, 0), rotation_matrix).transpose(3, 0, 1, 2)
+
+    @staticmethod
+    def _rotation_matrix(axis, angle):
+        cos, sin = math.cos(angle), math.sin(angle)
+        if axis == 0:
+            return np.array([[1, 0, 0], [0, cos, sin], [0, -sin, cos]])
+        elif axis == 1:
+            return np.array([[cos, 0, -sin], [0, 1, 0], [sin, 0, cos]])
+        elif axis == 2:
+            return np.array([[cos, sin, 0], [-sin, cos, 0], [0, 0, 1]])
+
+    @staticmethod
+    def gaussian_noise(data, mean=0, std=0.05, prob=0.5):
+        return data + np.random.normal(mean, std, size=data.shape) if random.random() < prob else data
+
+class JointBoneConversion(nn.Module):
+    def __init__(self, pairs):
+        super().__init__()
+        self.pairs = pairs
+
+    def forward(self, joint_data):
+        bone_data = np.zeros_like(joint_data)
         for v1, v2 in self.pairs:
-            bone[:, :, v1, :] = joint[:, :, v1, :] - joint[:, :, v2, :]
-        return bone
+            bone_data[:, :, v1, :] = joint_data[:, :, v1, :] - joint_data[:, :, v2, :]
+        return bone_data
 
-#改了图节点
-class bone2joint(nn.Module):
-    def __init__(self):
-        super(bone2joint, self).__init__()
-        self.center = 0
-        self.pairs_1 = [(10, 8)]
-        self.pairs_2 = [(8, 6), (9, 7), (7, 5)]
-        self.pairs_3 = [(15, 13), (13, 11), (16, 14), (14, 12)]
-        self.pairs_4 = [(11, 5), (12, 6), (11, 12)]
-        self.pairs_5 = [(5, 6), (5, 0), (6, 0)]
-        self.pairs_6 = [(1, 0), (2, 0), (3, 1), (4, 2)]
+class SkeletonAdaIN:
+    @staticmethod
+    def apply(input_data, reference_data, eps=1e-5):
+        bone_i = JointBoneConversion(joint_pairs).forward(input_data)
+        bone_r = JointBoneConversion(joint_pairs).forward(reference_data)
+        length_scale = (np.linalg.norm(bone_r, axis=0) + eps) / (np.linalg.norm(bone_i, axis=0) + eps)
+        return BoneToJointConverter().apply(bone_i * length_scale)
 
-    def __call__(self, bone, center):
-        joint = np.zeros_like(bone)
-        joint[:, :, self.center, :] = center
-        for v1, v2 in self.pairs_1:
-            joint[:, :, v1, :] = bone[:, :, v1, :] + joint[:, :, v2, :]
-        for v1, v2 in self.pairs_2:
-            joint[:, :, v1, :] = bone[:, :, v1, :] + joint[:, :, v2, :]
-        for v1, v2 in self.pairs_3:
-            joint[:, :, v1, :] = bone[:, :, v1, :] + joint[:, :, v2, :]
-        for v1, v2 in self.pairs_4:
-            joint[:, :, v1, :] = bone[:, :, v1, :] + joint[:, :, v2, :]
-        for v1, v2 in self.pairs_5:
-            joint[:, :, v1, :] = bone[:, :, v1, :] + joint[:, :, v2, :]
-        for v1, v2 in self.pairs_6:
-            joint[:, :, v1, :] = bone[:, :, v1, :] + joint[:, :, v2, :]
-        return joint
+class BoneToJointConverter:
+    def apply(self, bone_data, center_data):
+        joint_data = np.zeros_like(bone_data)
+        joint_data[:, :, self.center, :] = center_data
+        for pairs in self.pair_groups:
+            for v1, v2 in pairs:
+                joint_data[:, :, v1, :] += bone_data[:, :, v1, :] + joint_data[:, :, v2, :]
+        return joint_data
 
-def to_motion(input): # C T V M
-    C, T, V, M = input.shape
-    motion = np.zeros_like(input)
-    motion[:, :T - 1] = np.diff(input, axis=1)
-    return motion
+class GaussianBlur(nn.Module):
+    def __init__(self, channels=3, kernel_size=15, sigma_range=[0.1, 2], prob=0.5):
+        super().__init__()
+        self.channels, self.kernel_size, self.prob = channels, kernel_size, prob
+        radius = kernel_size // 2
+        self.kernel_index = np.arange(-radius, radius + 1)
+        self.sigma_range = sigma_range
+
+    def forward(self, x):
+        sigma = random.uniform(*self.sigma_range)
+        kernel = np.exp(-self.kernel_index ** 2 / (2.0 * sigma ** 2))
+        kernel = torch.from_numpy(kernel).unsqueeze(0).unsqueeze(0).repeat(self.channels, 1, 1, 1)
+        kernel = kernel / kernel.sum()
+        if random.random() < self.prob:
+            x = F.conv2d(x.permute(3, 0, 2, 1), kernel, padding=(0, self.kernel_size // 2), groups=self.channels)
+        return x.permute(1, -1, -2, 0).numpy()
+
+def to_motion(input_data):
+    motion_data = np.diff(input_data, axis=1, prepend=np.zeros_like(input_data[:, :1]))
+    return motion_data
